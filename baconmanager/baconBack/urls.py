55a42f0ad969f85dbc5db00875bd4885e3553cc1
Aliapoh <anu5200@hotmail.com>
2019-11-26 18:57:47 +0700

    added @POST

--- baconmanager/baconBack/urls.py
+++ baconmanager/baconBack/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns = [path('', index, name='index'), path('users/', list_user.as_view(), name="user_all")]
+urlpatterns = [path('', index, name='index'),
+               path('users/', list_user.as_view(), name="user_all"),
+               path('usersTest/', user_test)]
--- baconmanager/baconBack/views.py
+++ baconmanager/baconBack/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import generics, status
+
 from .models import User
 from .serializers import UserSerializer
 
@@ -9,6 +13,22 @@ from .serializers import UserSerializer
 
 def index(request):
     return HttpResponse("Hello.")
+
+@api_view(['GET', 'POST'])
+def user_test(request):
+
+    if request.method == 'GET':
+        qs = User.objects.all()
+        serializer = UserSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class list_user(generics.ListAPIView):
     queryset = User.objects.all()
