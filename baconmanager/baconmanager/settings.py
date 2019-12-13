816f2fd401a7042737bcf5de9990408957b4854e
Bob Lancer <janekarnkit2013@hotmail.com>
2019-12-13 17:19:31 +0700

    add static url

--- baconmanager/baconmanager/settings.py
+++ baconmanager/baconmanager/settings.py
@@ -118,4 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = "app-root/repo/wsgi/static"
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('assets', 'app-root/repo/wsgi/openshift/static'),
+
+)
