7a67e4e5b29d1317098bfb0a4aeda1641afc4eaa
Bob Lancer <janekarnkit2013@hotmail.com>
2019-12-13 17:00:23 +0700

    change settings.py

--- baconmanager/baconmanager/settings.py
+++ baconmanager/baconmanager/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'baconBack.apps.BaconbackConfig',
-    'rest_framework'
+    'djangorestframework'
 ]
 
 MIDDLEWARE = [
