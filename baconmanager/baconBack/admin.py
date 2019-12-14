922987fb3db0fa70ccc03330ff6131973734dd2b
Aliapoh <anu5200@hotmail.com>
2019-12-14 22:02:58 +0700

    updated admin page

--- baconmanager/baconBack/admin.py
+++ baconmanager/baconBack/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import *
 
 admin.site.register(User)
+admin.site.register(Attendance)
+admin.site.register(Session)
+admin.site.register(Member)
+admin.site.register(Class)
