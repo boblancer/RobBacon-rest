5b1b6da1d3fb6adbce387975162bcdd16f5ba951
Aliapoh <anu5200@hotmail.com>
2019-12-14 11:12:57 +0700

    Test

--- baconmanager/baconmanager/settings.py
+++ baconmanager/baconmanager/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -75,11 +75,10 @@ WSGI_APPLICATION = 'baconmanager.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
