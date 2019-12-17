576aa20fe7525c932a3a2c5e4a4bfad86601b190
lancer <janekarnkit2013@hotmail.com>
2019-12-17 22:16:36 +0700

    add cors whitelist

--- baconmanager/baconmanage/settings.py
+++ baconmanager/baconmanage/settings.py
@@ -137,8 +137,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3030',
+    'http://localhost:3000',
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
-    'http://localhost:3030',
+    'http://localhost:3000',
 ]
