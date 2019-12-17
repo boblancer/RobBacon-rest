308574351e70b83dae135e6233d017101db54b4d
lancer <janekarnkit2013@hotmail.com>
2019-12-18 00:05:12 +0700

    add module file

--- baconmanager/baconmanage/settings.py
+++ baconmanager/baconmanage/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'baconmanage.middleware.CorsMiddleware'
+    'baconmanager.baconmange.middleware.CorsMiddleware'
 
 ]
 
