ac9c554cfe0c9b4c7b9c5ec887d07a53ffba870e
lancer <janekarnkit2013@hotmail.com>
2019-12-17 21:31:51 +0700

    Add cors origin whitelist

--- baconmanager/baconmanage/settings.py
+++ baconmanager/baconmanage/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'baconmanager.baconBack.apps.BaconbackConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
 ROOT_URLCONF = 'baconmanager.baconmanage.urls'
@@ -130,3 +133,5 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CORS_ORIGIN_WHITELIST = 'localhost:3000',
