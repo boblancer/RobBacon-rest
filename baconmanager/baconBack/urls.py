38a87320ff92ac16dac15a979f6e3082cb5b08e2
Aliapoh <anu5200@hotmail.com>
2019-12-18 18:49:03 +0700

    changed <int:pk> to <string:pk>

--- baconmanager/baconBack/urls.py
+++ baconmanager/baconBack/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [path('', webhook, name='index'),
-               path('usersTest/<int:pk>', UserDetail.as_view(), name="UserDetailsAPI"),
+               path('usersTest/<string:pk>', UserDetail.as_view(), name="UserDetailsAPI"),
                path('usersTest/', UserList.as_view(), name="UserAPI")]
