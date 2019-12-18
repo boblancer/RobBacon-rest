0ccefaa878c17499b27f12b202c8bf7e8395dc72
Aliapoh <anu5200@hotmail.com>
2019-12-18 19:01:37 +0700

    fixed typo

--- baconmanager/baconBack/urls.py
+++ baconmanager/baconBack/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [path('', webhook, name='index'),
-               path('usersTest/<string:pk>', UserDetail.as_view(), name="UserDetailsAPI"),
-               path('usersTest/', UserList.as_view(), name="UserAPI")]
+               path('usersTest/', UserList.as_view(), name="UserAPI"),
+               path('usersTest/<str:pk>', UserDetail.as_view(), name="UserDetailsAPI")
+              ]
