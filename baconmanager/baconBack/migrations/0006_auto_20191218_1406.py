2cc0900ac686e5900d338586abbe7ce9f68b2c55
Aliapoh <anu5200@hotmail.com>
2019-12-18 21:11:56 +0700

    primarykey = false for classID

--- baconmanager/baconBack/migrations/0006_auto_20191218_1406.py
+++ baconmanager/baconBack/migrations/0006_auto_20191218_1406.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='attendance',
             name='classID',
-            field=models.IntegerField(),
+            field=models.IntegerField(primary_key=False),
         ),
     ]
 
