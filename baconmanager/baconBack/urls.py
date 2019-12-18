69fbe9a2937e4bc769d0f89366b8952ac862ae71
Aliapoh <anu5200@hotmail.com>
2019-12-18 18:42:34 +0700

    added <int:pk> to urls

--- baconmanager/baconBack/urls.py
+++ baconmanager/baconBack/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [path('', webhook, name='index'),
-               path('usersDetail/', UserDetail.as_view(), name="UserDetailsAPI"),
+               path('usersTest/<int:pk>', UserDetail.as_view(), name="UserDetailsAPI"),
                path('usersTest/', UserList.as_view(), name="UserAPI")]
