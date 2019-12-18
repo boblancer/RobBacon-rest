2630a6695ec3e45cdc379502b3cb9b5e0169d2e2
Aliapoh <anu5200@hotmail.com>
2019-12-18 19:19:25 +0700

    added attendance list

--- baconmanager/baconBack/urls.py
+++ baconmanager/baconBack/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [path('', webhook, name='index'),
                path('usersTest/', UserList.as_view(), name="UserAPI"),
                path('usersTest/<str:pk>', UserDetail.as_view(), name="UserDetailsAPI"),
+               path('Attendance/', AttendanceList.as_view(), name="AttendanceList"),
                path('Attendance/<int:pk>', AttendanceDetail.as_view(), name="AttendanceDetailsAPI")
               ]
