bc3e482fe5637fb475b4d2c020e0d51bf59fba67
lancer <janekarnkit2013@hotmail.com>
2019-12-19 17:59:17 +0700

    add middleware

--- baconmanager/baconBack/middleware.py
+++ baconmanager/baconBack/middleware.py
@@ -5,5 +5,7 @@ class MyMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        response['Access-Control-Allow-Origin'] = "https://bacon-attendance.firebaseapp.com"
+        response['Access-Control-Allow-Origin'] = "*"
+        # response['Access-Control-Allow-Origin'] = "https://bacon-attendance.firebaseapp.com"
+        response["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Authorization";
         return response
